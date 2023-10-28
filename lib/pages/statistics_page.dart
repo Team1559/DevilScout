e4c6c62e011311abe965d9691f28196fd2010658
nmartindev <nmartindevelopment@gmail.com>
2023-10-28 13:21:06 -0400

    Added new pages

---
+++ lib/pages/statistics_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class StatisticsPage extends StatefulWidget {
+  const StatisticsPage({Key? key}) : super(key: key);
+
+  @override
+  State<StatisticsPage> createState() => _StatisticsPageState();
+}
+
+class _StatisticsPageState extends State<StatisticsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Center(
+      child: Text('This is the Statistics Page'),
+    ));
+  }
+}
