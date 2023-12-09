63c8d500aafc89893158bf0f4953a802d20e2cf5
Nolan Martin <nmartindevelopment@gmail.com>
2023-12-09 14:43:21 -0500

    Implement placeholder UI and theme data

--- lib/pages/match_select_page.dart
+++ lib/pages/match_select_page.dart
@@ -11,9 +11,32 @@ class _MatchSelectPageState extends State<MatchSelectPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: const Text("DevilScout"),
-      centerTitle: true,
-    ));
+      appBar: AppBar(
+        title: const Text('Scouting'),
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.menu),
+        ),
+      ),
+      body: ListView.builder(
+        shrinkWrap: true,
+        itemCount: 5,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Text('Qualifier ${index + 1}'),
+                  const SizedBox(height: 10.0),
+                  const Text('1559 3010 6781'),
+                  const Text('4052 5678 3071'),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
