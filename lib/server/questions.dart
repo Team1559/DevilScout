61f0c3682ab7d1b9bbc95ae742dd90260f17132e
MathNerd28 <xander.bhalla@gmail.com>
2023-12-17 19:22:04 -0500

    Parse question types from JSON

--- lib/server/questions.dart
+++ lib/server/questions.dart
@@ -4,7 +4,7 @@ import 'server.dart';
 
 part 'questions.g.dart';
 
-@JsonEnum(fieldRename: FieldRename.screamingSnake)
+@JsonEnum(fieldRename: FieldRename.screamingSnake, alwaysCreate: true)
 enum QuestionType {
   boolean,
   counter,
@@ -16,20 +16,95 @@ enum QuestionType {
   single;
 }
 
-@JsonSerializable(createToJson: false)
 class Question {
   final String prompt;
   final QuestionType type;
-  final Map<String, Object>? config;
+  final Object config;
 
-  Question({
+  const Question({
     required this.prompt,
     required this.type,
     required this.config,
   });
 
-  factory Question.fromJson(Map<String, dynamic> json) =>
-      _$QuestionFromJson(json);
+  factory Question.fromJson(Map<String, dynamic> json) {
+    String prompt = json['prompt'];
+    QuestionType type = $enumDecode(_$QuestionTypeEnumMap, json['type']);
+
+    Map<String, dynamic> configJson = json['config'] ?? {};
+    Object? config = switch (type) {
+      QuestionType.boolean => _$BooleanConfigFromJson(configJson),
+      QuestionType.counter => _$CounterConfigFromJson(configJson),
+      QuestionType.grid => _$GridConfigFromJson(configJson),
+      QuestionType.multiple => _$MultipleChoiceConfigFromJson(configJson),
+      QuestionType.number => _$NumberInputConfigFromJson(configJson),
+      QuestionType.range => _$NumberRangeConfigFromJson(configJson),
+      QuestionType.sequence => _$SequenceConfigFromJson(configJson),
+      QuestionType.single => _$SingleChoiceConfigFromJson(configJson),
+    };
+
+    return Question(prompt: prompt, type: type, config: config);
+  }
+}
+
+@JsonSerializable(createToJson: false)
+class BooleanConfig {
+  const BooleanConfig();
+}
+
+@JsonSerializable(createToJson: false)
+class CounterConfig {
+  const CounterConfig();
+}
+
+@JsonSerializable(createToJson: false)
+class GridConfig {
+  final List<String> options;
+  final int height;
+  final int width;
+
+  const GridConfig({
+    required this.options,
+    required this.height,
+    required this.width,
+  });
+}
+
+@JsonSerializable(createToJson: false)
+class MultipleChoiceConfig {
+  final List<String> options;
+
+  const MultipleChoiceConfig({required this.options});
+}
+
+@JsonSerializable(createToJson: false)
+class NumberInputConfig {
+  final int min;
+  final int max;
+
+  const NumberInputConfig({required this.min, required this.max});
+}
+
+@JsonSerializable(createToJson: false)
+class NumberRangeConfig {
+  final int min;
+  final int max;
+
+  const NumberRangeConfig({required this.min, required this.max});
+}
+
+@JsonSerializable(createToJson: false)
+class SequenceConfig {
+  final List<String> options;
+
+  const SequenceConfig({required this.options});
+}
+
+@JsonSerializable(createToJson: false)
+class SingleChoiceConfig {
+  final List<String> options;
+
+  const SingleChoiceConfig({required this.options});
 }
 
 @JsonSerializable(createToJson: false)
